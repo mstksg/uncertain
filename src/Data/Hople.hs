34cf8e6b3b89661bf9d59b810e55e544f9d3ddba
Justin Le <justin@jle.im>
2016-04-27 01:37:15 -0700

    adding show instance

--- src/Data/Hople.hs
+++ src/Data/Hople.hs
@@ -12,17 +12,17 @@ module Data.Hople
   where
 
 data H1 a = H1 !a
-  deriving (Functor, Foldable, Traversable)
+  deriving (Functor, Foldable, Traversable, Show)
 
 data H2 a = H2 !a !a
-  deriving (Functor, Foldable, Traversable)
+  deriving (Functor, Foldable, Traversable, Show)
 
 data H3 a = H3 !a !a !a
-  deriving (Functor, Foldable, Traversable)
+  deriving (Functor, Foldable, Traversable, Show)
 
 data H4 a = H4 !a !a !a !a
-  deriving (Functor, Foldable, Traversable)
+  deriving (Functor, Foldable, Traversable, Show)
 
 data H5 a = H5 !a !a !a !a !a
-  deriving (Functor, Foldable, Traversable)
+  deriving (Functor, Foldable, Traversable, Show)
 
